231b211c606c0b6a893b1437d0bde4fb9449a9b2
kunsir <825464572@qq.com>
2018-03-13 23:32:12 +0800

    Init commit

---
+++ HelloWorld/urls.py
@@ -0,0 +1,7 @@
+# urls.py
+from django.conf.urls import url
+from django.contrib import admin
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+]
---
+++ TestModel/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from TestModel.models import Test,Contact,Tag
+
+# Register your models here.
+class TagInline(admin.TabularInline):
+    model = Tag
+
+class ContactAdmin(admin.ModelAdmin):
+    inlines = [TagInline]  # Inline
+    fieldsets = (
+        ['Main',{
+            'fields':('name','email'),
+        }],
+        ['Advance',{
+            'classes': ('collapse',),
+            'fields': ('age',),
+        }]
+
+    )
+
+admin.site.register(Contact, ContactAdmin)
+admin.site.register([Test])
